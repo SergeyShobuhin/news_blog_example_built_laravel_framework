155ce63f7cd048ab75625361c88a5d5feebd4432
SergeyShobuhin <shobuhin@mail.ru>
2025-02-26 20:04:27 +0700

    add pagination

---
+++ database/factories/BlogFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Blog>
+ */
+class BlogFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->realText(20),
+            'description' => $this->faker->text(50),
+            'content' => $this->faker->text(),
+            'category_id' => Category::get()->random()->id,
+
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Blog;
+use App\Models\Category;
+use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Category::factory(5)->create();
+        $tags = Tag::factory(3)->create();
+        $blogs = Blog::factory(100)->create();
+
+        foreach ($blogs as $blog) {
+            $randKey = rand(1,3);
+            $tagIds = $tags->random($randKey)->pluck('id');
+            $blog->tags()->attach($tagIds);
+        }
+
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
     }
 }
